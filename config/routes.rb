d46d54a2247eaae6437bd23032b3b1f1695a6d7f
Dmitry Ovcharenko <ovcharenkodmitry@gmail.com>
2010-08-12 13:25:44 +0600

    123

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
-   map.resources :product 
+
+   map.resources :product
 
   # The priority is based upon order of creation: first created -> highest priority.
 
